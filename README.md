641182c84a6d5e99315b80f35a8408861a4a8896
pasaperez <aperez@slingr.io>
2023-03-03 18:06:55 -0300

    Mejora de API Rest y Readme

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # API-REST-Test
 Api Rest in NodeJs With Express
 
+# Installation
+## Download Repository
+```bash
+git clone https://github.com/pasaperez-slingr/API-REST-Test.git
+```
 
-#Starting Services
 
+## Install Dependencies
+
+```bash
+npm install
+```
+
+# Starting Services
+
+```bash
 npm start
+```
 
-#Test Paths
+# Test Paths
 
+## Para cookies:
 
 http://localhost:30000/getCookies
 
@@ -17,3 +32,24 @@ http://localhost:30000/setCookie/test
 
 http://localhost:30000/clearCookie/test
 
+
+### From Platform Console
+```javascript
+log(JSON.stringify(app.endpoints.http.get("http://127.0.0.1:30000/getCookies")))
+```
+
+```javascript
+log(JSON.stringify(app.endpoints.http.get({path: "http://127.0.0.1:30000/getCookies", forceDisableCookies: "true"})))
+```
+
+```javascript
+log(JSON.stringify(app.endpoints.http.get({path: "http://127.0.0.1:30000/setCookie/cookie1", forceDisableCookies: "true"})))
+```
+
+```javascript
+log(JSON.stringify(app.endpoints.http.get({path: "http://127.0.0.1:30000/getCookies", forceDisableCookies: "false"})))
+```
+
+```javascript
+log(JSON.stringify(app.endpoints.http.get({path: "http://127.0.0.1:30000/setCookie/cookie1", forceDisableCookies: "false"})))
+```
--- app/routes/methods.routes.ts
+++ app/routes/methods.routes.ts
@@ -1,12 +1,12 @@
 export function APIRest (server: { get: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; post: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; put: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; patch: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; del: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; head: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; opts: (arg0: string, arg1: (_: any, res: any, next: any) => void) => void; }) {
 
-  server.get('/get', (_: any, res: any, next: any) => { res.send('GET'); next(); });
-  server.post('/post', (_: any, res: any, next: any) => { res.send('POST'); next(); });
-  server.put('/put', (_: any, res: any, next: any) => { res.send('PUT'); next(); });
-  server.patch('/patch', (_: any, res: any, next: any) => { res.send('PATCH'); next(); });
-  server.del('/delete', (_: any, res: any, next: any) => { res.send('DELETE'); next(); });
-  server.head('/head', (_: any, res: any, next: any) => { res.send('HEAD'); next(); });
-  server.opts('/options', (_: any, res: any, next: any) => { res.send('OPTIONS'); next(); });
+  server.get('/method', (_: any, res: any, next: any) => { res.send({res: 'GET'}); next(); });
+  server.post('/method', (_: any, res: any, next: any) => { res.send({res: 'POST'}); next(); });
+  server.put('/method', (_: any, res: any, next: any) => { res.send({res: 'PUT'}); next(); });
+  server.patch('/method', (_: any, res: any, next: any) => { res.send({res: 'PATCH'}); next(); });
+  server.del('/method', (_: any, res: any, next: any) => { res.send({res: 'DELETE'}); next(); });
+  server.head('/method', (_: any, res: any, next: any) => { res.send({res: 'HEAD'}); next(); });
+  server.opts('/method', (_: any, res: any, next: any) => { res.send({res: 'OPTIONS'}); next(); });
 
   return server;
 }
